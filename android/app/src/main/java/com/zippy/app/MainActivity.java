45175a15c616ad45e40175f0bf5e138ab4b69a6b
Dostanfarms <chandrasekhar3866@gmail.com>
2026-02-05 07:16:59 +0000

    m

---
+++ android/app/src/main/java/com/zippy/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zippy.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
  import type { CapacitorConfig } from '@capacitor/cli';
  
  const config: CapacitorConfig = {
-   appId: 'app.lovable.272be06f1428431096298c2f21b87333',
-   appName: 'zippydelivary',
+   appId: 'com.zippy.app',
+   appName: 'Zippy',
    webDir: 'dist',
    server: {
      url: 'https://272be06f-1428-4310-9629-8c2f21b87333.lovableproject.com?forceHideBadge=true',
